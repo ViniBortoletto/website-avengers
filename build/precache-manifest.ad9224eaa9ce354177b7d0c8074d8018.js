31a8a9c6358db1826719676bfd8cd281ced3efe7
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2019-11-11 13:35:18 -0200

    build created

---
+++ build/precache-manifest.ad9224eaa9ce354177b7d0c8074d8018.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69434fec082df434e3b70facd7e14959",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9c0497e6a403d8783896",
+    "url": "/static/js/2.c20865f7.chunk.js"
+  },
+  {
+    "revision": "8202b73c49d81462c0ec",
+    "url": "/static/js/main.a41a89d2.chunk.js"
+  },
+  {
+    "revision": "767d15b2268a298d670a",
+    "url": "/static/js/runtime-main.4765d705.js"
+  },
+  {
+    "revision": "9bad0c0910065181198874cd4ba6960e",
+    "url": "/static/media/bg_home.9bad0c09.jpg"
+  },
+  {
+    "revision": "4ee28210178f2a57621e2cb59b3ae6a1",
+    "url": "/static/media/icon_footer-vbLogo.4ee28210.png"
+  },
+  {
+    "revision": "5a6d6040fc101ebf8d04375d3d2fe3ab",
+    "url": "/static/media/icon_home-big.5a6d6040.png"
+  }
+]);
